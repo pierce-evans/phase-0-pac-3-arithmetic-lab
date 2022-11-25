d3591e942ed5e0d8063e57589b7e73c68b970e8c
Pierce <pierceevans@me.com>
2022-11-25 15:32:24 -0600

    commit tree 534655782f6bbfcb14732175c65e6f341135711c

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+return a + b;
+}
+function subtract(a, b) {
+return a - b;
+}
+function multiply (a, b) {
+return a * b;
+}
+function divide(a, b) {
+return a / b;
+}
+function increment(a) {
+    return a = a + 1;
+}
+function decrement(b) {
+    return b = b - 1;
+}
+function makeInt(a) {
+    return parseInt (a.toString(), 10);
+}
+function preserveDecimal(a) {
+    return parseFloat (a);
+}
